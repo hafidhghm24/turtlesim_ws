1f281515dba44accb859bf28a919763939a7f65f
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-06-30 21:32:30 +0200

    ajout du spawn des tortue sur lecran et du deplacement de la tortue principale vers des coordonée

--- src/turtlesim_cpp_pkg/src/alive.cpp
+++ src/turtlesim_cpp_pkg/src/alive.cpp
@@ -1,26 +1,76 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/srv/spawn.hpp"
+#include <random>
+#include <string>
+
+using namespace std::chrono_literals;
+using namespace std::placeholders;
+
 /*
     @brief spawn turtles on the window, and to manage which turtle is still “alive” (on the screen).
-    TODO: apelle du service /spawn pour creer les tortues 
-    TODO: apelle du service /kill pour retirer les tortues
-    TODO: publisher sur /alive_turteles de la liste des tortues en vie 
-    TODO: Handle a service server to “catch” a turtle, which means to call the /kill service and remove the turtle from the array of alive turtles.
 */
-class turtle_spawner : public rclcpp::Node{
+class turtle_spawner : public rclcpp::Node {
 public:
-    turtle_spawner() : Node("alive_node"){
-        
+    // CORRECTION : Le membre s'appelle turtle_name_prefix_ (avec l'underscore à la fin)
+    turtle_spawner() : Node("alive_node"), turtle_name_prefix_("turtle"), turtle_compteur(0) {
+        turtle_client_ = this->create_client<turtlesim::srv::Spawn>("/spawn");
+        RCLCPP_WARN(this->get_logger(), "Creation du client turtle_spawner!");
+
+        timer_ = this->create_wall_timer(5s, std::bind(&turtle_spawner::random_arguments_spawn, this));
     }
     
 private:
+    void random_arguments_spawn() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dist(0.0, 11.0);
+        std::uniform_real_distribution<float> angle_dist(0.0, 6.28); 
+        
+        
+        std::string turtle_name = this->turtle_name_prefix_ + std::to_string(this->turtle_compteur);
+        
+        float x = dist(gen);
+        float y = dist(gen);
+        float theta = angle_dist(gen);
+        
+        spowner_call(x, y, theta, turtle_name);
+        this->turtle_compteur++;
+    }
 
+    void spowner_call(float x, float y, float theta, std::string turtle_name) {
+        if(!turtle_client_->wait_for_service(1s)) {
+            RCLCPP_WARN(this->get_logger(), "attente du service /spawn...");
+            return;
+        }
+
+        auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
+        
+
+        request->name = turtle_name;
+        request->x = x;
+        request->y = y;
+        request->theta = theta;
+
+        turtle_client_->async_send_request(request, std::bind(&turtle_spawner::spowner_callback, this, _1));
+    }
+
+    void spowner_callback(rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture future) {
+        auto response = future.get();
+        RCLCPP_INFO(this->get_logger(), "nom: %s", response->name.c_str());
+    }
+
+    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr turtle_client_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    std::string turtle_name_prefix_;
+    int turtle_compteur;
 };
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<turtle_spawner>();
 
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
--- src/turtlesim_cpp_pkg/src/turtle1.cpp
+++ src/turtlesim_cpp_pkg/src/turtle1.cpp
@@ -1,101 +1,112 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/msg/pose.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include <cmath>
+#include <cmath> 
 
 const double PI = 3.141592653589793;
-/*
-    @brief A custom node to control the turtle (named “turtle1”) which is already spawned in the turtlesim_node.
-    TODO: Run a control loop (for example using a timer with a high rate) to reach a given target point. The first turtle on the screen “turtle1” will be the “master” turtle to control. To control the turtle you can subscribe to /turtle1/pose and publish to /turtle1/cmd_vel.
-    TODO: The control loop will use a simplified P controller.
-    TODO: Subscribe to the /alive_turtles topic to get all current turtles with coordinates. From that information, select a target turtle (turtle to catch).
-    TODO: When a turtle has been caught by the master turtle, call the service /catch_turtle advertised by the turtle_spawner node.
-*/
-class turtle_controller : public rclcpp::Node{
+
+class turtle_controller : public rclcpp::Node {
 public:
-    turtle_controller() : Node("controller_node"){
-        //parametre de cible temporaire
+    turtle_controller() : Node("controller_node") {
+
         this->declare_parameter("Xtarget", 8.0);
-        target_x = this->get_parameter("Xtarget").as_double();
         this->declare_parameter("Ytarget", 4.0);
+
+        target_x = this->get_parameter("Xtarget").as_double();
         target_y = this->get_parameter("Ytarget").as_double();
 
-        turtle1_pose_sub_ = this->create_subscription<turtlesim::msg::Pose>("/turtle1/pose", 10, std::bind(&turtle_controller::position_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "subscription au topic /pose !");
+        turtle1_pose_sub_ = this->create_subscription<turtlesim::msg::Pose>(
+            "/turtle1/pose", 10, 
+            std::bind(&turtle_controller::position_callback, this, std::placeholders::_1));
+        
+        RCLCPP_INFO(this->get_logger(), "Abonnement au topic /turtle1/pose établi !");
 
-        turtle1_cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
-        RCLCPP_INFO(this->get_logger(), "publisher sur /cmd_vel created!");
+        turtle1_cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+            "/turtle1/cmd_vel", 10);
+        
+        RCLCPP_INFO(this->get_logger(), "Publisher sur /turtle1/cmd_vel créé !");
 
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&turtle_controller::controller_callback, this));
-
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100), 
+            std::bind(&turtle_controller::controller_callback, this));
     }
-    
+
 private:
-    void position_callback(const turtlesim::msg::Pose pose_msg){
-        this-> pose_ = pose_msg;
-        RCLCPP_INFO(this->get_logger(), "position:(x=%f; y=%f; theta=%f)", pose_msg.x, pose_msg.y, pose_msg.theta);
-        bool pose_received_ = true;
+    void position_callback(const turtlesim::msg::Pose pose_msg) {
+
+        this->pose_ = pose_msg;
+        
+
+        this->pose_received_ = true;
     }
 
-    void controller_callback(){
+    void controller_callback() {
+
+        if (!pose_received_) {
+            return;
+        }
+
         auto cmd_msg = geometry_msgs::msg::Twist();
 
-        if (!pose_received_) return;
-
-        /*TODO: equations distance cible*/
         dist_x = target_x - pose_.x;
         dist_y = target_y - pose_.y;
-        distance = std::sqrt((dist_x * dist_x) + (dist_y * dist_y));
         
-        /*si la distance a la cible est trop grande*/
-        if(distance > 0.5){
-            cmd_msg.linear.x = distance;
+        distance = std::sqrt((dist_x * dist_x) + (dist_y * dist_y));
 
+        if (distance > 0.1) {
 
-            /*TODO: equation orientation cible*/
-            target_angle = std::atan2(dist_x, dist_y);
+            cmd_msg.linear.x = 2.0 * distance;
+
+            target_angle = std::atan2(dist_y, dist_x);
             angle_diff = target_angle - pose_.theta;
-            if(angle_diff > PI){
-                angle_diff -= 2*PI;
-            }
-            else if(angle_diff < -PI){
-                angle_diff += 2*PI;
+
+            if (angle_diff > PI) {
+                angle_diff -= 2.0 * PI;
+            } else if (angle_diff < -PI) {
+                angle_diff += 2.0 * PI;
             }
 
-            cmd_msg.angular.z = angle_diff;
-        }
-
-        else{
+            cmd_msg.angular.z = 6.0 * angle_diff;
+        } 
+        else {
             cmd_msg.linear.x = 0.0;
             cmd_msg.angular.z = 0.0;
+            RCLCPP_INFO(this->get_logger(), "Cible atteinte!");
         }
-        
-        
-        
-        turtle1_cmd_pub_->publish(cmd_msg);
-        RCLCPP_INFO(this->get_logger(), "/cmd_val => (forward_speed = %f; backward_speed= %f; turn_speed= %f)");
+
+        this->turtle1_cmd_pub_->publish(cmd_msg);
+
+        RCLCPP_INFO(
+            this->get_logger(), 
+            "/cmd_vel => Vitesse Linéaire = %.2f m/s, Vitesse Angulaire = %.2f rad/s",
+            cmd_msg.linear.x,
+            cmd_msg.angular.z
+        );
     }
+
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr turtle1_pose_sub_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr turtle1_cmd_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
+
     turtlesim::msg::Pose pose_;
     bool pose_received_ = false;
     double distance;
     float target_angle;
     float angle_diff;
 
-    /*cible temporaire*/
+    // Cible
     float dist_x;
     float dist_y;
     double target_x;
     double target_y;
-    
 };
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
+    
     auto node = std::make_shared<turtle_controller>();
-
+    rclcpp::spin(node);
+    
     rclcpp::shutdown();
     return 0;
 }
